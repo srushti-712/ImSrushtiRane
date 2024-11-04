c80c98022e6e1b55df44ddf30df77c8b3899efa3
Srushti Rane <srushtirane@Srushtis-MacBook-Air.local>
2024-11-04 10:31:29 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
